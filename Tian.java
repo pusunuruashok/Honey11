5e228ad07cf47b483dd7b44b4b4c55a49b1fb153
Ashok Pusunuru <pusunuruashok99@gmail.com>
2026-04-22 23:12:13 -0400

    the designed bussiness

---
+++ Tian.java
@@ -0,0 +1,7 @@
+public class Tian {
+    public static void main(String[] args) {
+        System.out.println("Hello, Tian!");
+        System.out.println("Welcome to Java programming."); 
+        
+    }
+}
